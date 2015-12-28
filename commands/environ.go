9708cceaaa2f6f26772faf87372c3667288b98ce
David Blackmon <davidkblackmon@gmail.com>
2015-12-28 14:26:11 -0500

    add some help for env vars

---
+++ commands/environ.go
@@ -0,0 +1,23 @@
+package commands
+
+import (
+  "fmt"
+  "github.com/spf13/cobra"
+)
+
+func init() {
+  EwaCmd.AddCommand(environCmd)
+}
+
+var environCmd = &cobra.Command{
+  Use: "environ",
+  Short: "list the environment variables that control Ewa",
+  Long: "list the environment variables that control Ewa",
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println( `EWA_LOGDESTINATION - controls where logs are written
+  allowable values are NONE, STDOUT, FILE, BOTH. Defaults to FILE
+EWA_DATADIR - sets the directory Ewa uses to store data. Defaults to home dir of current user
+EWA_LOGLOCATION - sets the directory Ewa uses to store logs. Defaults to EWA_DATADIR`)
+  },
+}
+
